d1f47015dfdddf963a760447374133341f98a51b
stefanofortu <stefano.fortunati@saleri.com>
2025-12-21 17:33:38 +0100

    added logging

--- Classes/MainWindows.py
+++ Classes/MainWindows.py
@@ -1,6 +1,7 @@
 from PySide6.QtCore import Qt, QSize
 from PySide6.QtGui import QIcon, QAction
-from PySide6.QtWidgets import QMainWindow, QWidget, QTabWidget, QToolBar, QStatusBar, QVBoxLayout
+from PySide6.QtWidgets import QMainWindow, QWidget, QTabWidget, QToolBar, QStatusBar, QVBoxLayout, QTextBrowser, \
+    QHBoxLayout, QPushButton
 
 from Classes.Configuration_Data import Configuration_Data
 from Classes.MDF_Elaboration_Widget import MDF_Elaboration_Widget
@@ -12,8 +13,10 @@ from Classes.MDF_Elaboration_Widget import MDF_Elaboration_Widget
 from Classes.CSV_Creator_Widget import CSV_Creator_Widget
 
 from icons.resources import resource_path
-import logging
+import logging, os
 
+from Classes.LoggingStream import df_logger
+logger = logging.getLogger("SIL_TOOLS")
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -40,7 +43,7 @@ class MainWindow(QMainWindow):
 
         toolbar_action_open = QAction(QIcon(resource_path("open.jpg")), "Open", self)
         toolbar_action_open.setStatusTip("Open existing configuration")
-        #toolbar_action_open.triggered.connect(self.open_configuration_file)
+        toolbar_action_open.triggered.connect(self.open_configuration_file)
 
         toolbar_action_save = QAction(QIcon(resource_path("save.ico")), "Save", self)
         toolbar_action_save.setStatusTip("Save current configuration")
@@ -75,18 +78,41 @@ class MainWindow(QMainWindow):
         tab_widget.setTabPosition(QTabWidget.North)
         tab_widget.setMovable(False)
 
-        tab_widget.insertTab(0, self.mdf_creator_widget, "MDF Creator")
-        tab_widget.insertTab(1, self.mdf_elaboration_widget, "MDF Elaboration")
-        tab_widget.insertTab(2, self.csv_creator_widget, "CSV Creator")
-        tab_widget.insertTab(3, self.pwm_reader_widget, "Arduino set Conversions")
+        if self.configuration_data.tab_mdf_conversion:
+            tab_widget.insertTab(0, self.mdf_creator_widget, "MDF Creator")
+        if self.configuration_data.tab_mdf_elaboration:
+            tab_widget.insertTab(1, self.mdf_elaboration_widget, "MDF Elaboration")
+        if self.configuration_data.tab_csv_conversion:
+            tab_widget.insertTab(2, self.csv_creator_widget, "CSV Creator")
+        if self.configuration_data.tab_arduino:
+            tab_widget.insertTab(3, self.pwm_reader_widget, "Arduino set Conversions")
+
+        ##### Set main layout
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(tab_widget)
+        #main_layout.addWidget(logTextBox.widget)
 
         #logTextBox = QTextEditLogger(self)
         #logging.getLogger().addHandler(logTextBox)
 
-        # Set main layout
-        main_layout = QVBoxLayout()
-        main_layout.addWidget(tab_widget)
-        #main_layout.addWidget(logTextBox.widget)
+        ############### LOGGING  ###############
+        self.logging_text_browser = QTextBrowser(self)
+        # LoggingStream.stdout().messageWritten.connect(self.logging_text_browser.insertPlainText)
+        # LoggingStream.stderr().messageWritten.connect(self.logging_text_browser.insertPlainText)
+        for handler in logger.handlers:
+            if hasattr(handler, "name"):
+                if handler.name == "qt_text_browser_logging":
+                    handler.add_widget(self.logging_text_browser)
+        #df_logger.log_signal.connect(self.append_log)
+        main_layout.addWidget(self.logging_text_browser)
+        ############### CLEAR BUTTON  ###############
+        clear_row_layout = QHBoxLayout()
+        clear_row_layout.addStretch()
+        btn_clear_window = QPushButton("Clear window")
+        btn_clear_window.setIcon(QIcon(resource_path('cleanup-icon-small.jpg')))
+        btn_clear_window.pressed.connect(self.clear_window)
+        clear_row_layout.addWidget(btn_clear_window)
+        main_layout.addLayout(clear_row_layout)
 
         # Serve un layout a cui assegnare il layout
         main_widget = QWidget()
@@ -98,6 +124,7 @@ class MainWindow(QMainWindow):
 
     def open_configuration_file(self):
         try:
+            logger.warning("Ciao")
             print("to be replaced")
             #hil_function_file_data, tc_highlight_data, tc_substitution_data = self.configuration_file.open()
             #self.tc_substitution_widget.update_handler(tc_substitution_data)
@@ -124,3 +151,10 @@ class MainWindow(QMainWindow):
         #                             tc_highlight_data=tc_highlight_data.return_json_dict(),
         #                             tc_substitution_data=tc_substitution_data.return_json_dict(),
         #                             select_new_file=True)
+
+    def clear_window(self):
+        self.logging_text_browser.clear()
+
+    def append_log(self, log_text: str):
+        """Slot to update QTextBrowser."""
+        self.logging_text_browser.append(f"{log_text}")
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import logging
+from Classes.LoggingStream import setup_logger
+
 import os
 import sys
 from PySide6.QtWidgets import QApplication
@@ -7,17 +9,17 @@ from Classes.MainWindows import MainWindow
 import qdarkstyle
 #from Arduino.python.MainWindows_temp import MainWindow
 from Classes.QTextEditLogger import logging_setup
-
+logger = logging.getLogger("SIL_TOOLS")
 
 if __name__ == '__main__':
-    #logging_setup()
+    setup_logger()
 
     if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
-        logging.debug('PyInstaller bundle running')
-        logging.debug("sys._MEIPASS : %s", sys._MEIPASS)
+        logger.debug('PyInstaller bundle running')
+        logger.debug("sys._MEIPASS : %s", sys._MEIPASS)
         print(sys._MEIPASS)
     else:
-        logging.debug('running in a normal Python process')
+        logger.debug('running in a normal Python process')
     print(PySide6.__version__)
 
     #sys.argv += ['-platform', 'windows:darkmode=1']
